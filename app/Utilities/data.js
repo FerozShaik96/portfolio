594dd3a2f75eb3f5b44cbaa5b351bda4750d6709
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-23 23:40:04 +0530

    added Required Data for Leadership section

--- app/Utilities/data.js
+++ app/Utilities/data.js
@@ -1,3 +1,13 @@
+import imag1 from '@/public/img1.png';
+import imag2 from '@/public/img2.png';
+import imag3 from '@/public/img3.png';
+import imag4 from '@/public/img4.png';
+import imag5 from '@/public/img5.png';
+import imag6 from '@/public/img6.png';
+import imag7 from '@/public/img7.png';
+import imag8 from '@/public/img8.png';
+import imag9 from '@/public/img9.png';
+import imag10 from '@/public/img10.png';
 export const AboutData = {
   title: 'About Me',
   size: 350,
@@ -31,4 +41,21 @@ export const getInTouch = {
   message:
     "I'm currently looking for full-time React Developer/Frontend Developer opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
   email: 'khansaif59@gmail.com',
+};
+export const imgData = [
+  imag1,
+  imag2,
+  imag3,
+  imag4,
+  imag5,
+  imag6,
+  imag7,
+  imag8,
+  imag9,
+  imag10,
+];
+export const leadershipData = {
+  heading: 'Leadership',
+  message:
+    'As a React developer with leadership experience, I bring technical expertise, project management skills, and strong communication abilities to the table. I have a proven track record of building large-scale applications, optimizing code for performance, and implementing best practices. Additionally, I have experience leading teams and projects, creating and maintaining project plans, managing timelines and budgets, and ensuring timely delivery of high-quality work. I excel at communicating effectively with team members, stakeholders, and clients, both verbally and in writing, and am a skilled listener who can provide clear and constructive feedback. I am also passionate about mentoring and training other developers to help them develop their skills and achieve their goals. With my ability to solve complex technical problems and think creatively, I am always up-to-date with the latest developments in React and related technologies, and am able to innovate and inspire a positive, collaborative work environment.',
 };
