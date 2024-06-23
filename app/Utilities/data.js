b43290357b1876d3be727cf19b50a990dea52de3
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-23 16:29:32 +0530

    added some dummy project Data

--- app/Utilities/data.js
+++ app/Utilities/data.js
@@ -4,3 +4,8 @@ export const AboutData = {
   message:
     "My name is Mohd Shahid Iqbal. I’m a post-graduate of 2020 from Jamia hamdard University at New Delhi with a degree in Masters of Computer Application . I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
 };
+export const data = [
+  { Javascript: "51.7 %" },
+  { CSS: "33.7 %" },
+  { HTML: "17.7 %" },
+];
