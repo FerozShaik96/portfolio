74c86e88489f7386bdc59668e419e2c643ca0c9e
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-23 18:44:24 +0530

    Added Data required for  Experience Page

--- app/Utilities/data.js
+++ app/Utilities/data.js
@@ -1,11 +1,28 @@
 export const AboutData = {
-  title: "About Me",
+  title: 'About Me',
   size: 350,
   message:
     "My name is Mohd Shahid Iqbal. I’m a post-graduate of 2020 from Jamia hamdard University at New Delhi with a degree in Masters of Computer Application . I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
 };
 export const data = [
-  { Javascript: "51.7 %" },
-  { CSS: "33.7 %" },
-  { HTML: "17.7 %" },
+  { Javascript: '51.7 %' },
+  { CSS: '33.7 %' },
+  { HTML: '17.7 %' },
+];
+export const experienceData = [
+  {
+    role: 'React Developer',
+    about: 'simpana Technologies',
+    duration: 'Aug 2020 - May 2022 ',
+  },
+  {
+    role: 'Senior Software Engineer ',
+    about: 'Vendify',
+    duration: 'May 2022 - March 2023',
+  },
+  {
+    role: 'React Developer',
+    about: 'pace',
+    duration: 'March 2023 - Present ',
+  },
 ];
