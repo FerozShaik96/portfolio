2aeb317416305f73a5783ce9371cf27b5d103a64
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-23 20:36:55 +0530

    Added Custom Font to Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        custom: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Oxygen"',
+          'Ubuntu',
+          '"Cantarell"',
+          '"Fira Sans"',
+          '"Droid Sans"',
+          '"Helvetica Neue"',
+          'sans-serif',
+        ],
+      },
       colors: {
-        "gray-light": "#6C757D",
-        "gray-text": "#212529",
-        "gray-back": "#F8F9FA",
-        "gray-lan": "#A5A8AA",
-        "gray-About": "#E9ECEF",
+        'gray-light': '#6C757D',
+        'gray-text': '#212529',
+        'gray-back': '#F8F9FA',
+        'gray-lan': '#A5A8AA',
+        'gray-About': '#E9ECEF',
+        'gray-footer': '#F5F5F5',
       },
     },
   },
