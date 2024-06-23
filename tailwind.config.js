2ab0ffe7faeee1f152783301fd55e7cb40778acc
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-23 16:22:17 +0530

    Removed some files and configured Tailwind For Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "gray-light": "#6C757D",
+        "gray-text": "#212529",
+        "gray-back": "#F8F9FA",
+        "gray-lan": "#A5A8AA",
+        "gray-About": "#E9ECEF",
       },
     },
   },
