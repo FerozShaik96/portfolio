0b174e741a34645630d948ed29cc42564e03626a
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-23 16:30:17 +0530

    Rendered Project

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import About from "./components/About/About";
+import Project from "./components/Projects/Projects";
 
 export default function Home() {
   return (
     <main>
       <About />
+      <Project />
     </main>
   );
 }
