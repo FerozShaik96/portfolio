43e8ecccb34e1d766dc9a117b444176ff51117ff
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-25 02:20:50 +0530

    rendering

--- app/page.js
+++ app/page.js
@@ -3,10 +3,12 @@ import Experience from '@/app/components/Experience/Experience';
 import GetInTouch from '@/app/components/Footer/GetInTouch';
 import Leadership from '@/app/components/LeaderShip/LeaderShip';
 import Project from '@/app/components/Projects/Projects';
+import Navigation from '@/app/components/Navigation/Navigation';
 import Skills from '@/app/components/Skill/Skills';
 export default function Home() {
   return (
     <main>
+      <Navigation />
       <About />
       <Experience />
       <Project />
