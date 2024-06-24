df502b48deb67375a5a7aaa3ff9cedba8843469a
FerozShaik96 <ferozshaik1997@gmail.com>
2024-06-24 17:09:52 +0530

    Added Skills Data and Installed Progress bar react package

--- app/Utilities/data.js
+++ app/Utilities/data.js
@@ -59,3 +59,25 @@ export const leadershipData = {
   message:
     'As a React developer with leadership experience, I bring technical expertise, project management skills, and strong communication abilities to the table. I have a proven track record of building large-scale applications, optimizing code for performance, and implementing best practices. Additionally, I have experience leading teams and projects, creating and maintaining project plans, managing timelines and budgets, and ensuring timely delivery of high-quality work. I excel at communicating effectively with team members, stakeholders, and clients, both verbally and in writing, and am a skilled listener who can provide clear and constructive feedback. I am also passionate about mentoring and training other developers to help them develop their skills and achieve their goals. With my ability to solve complex technical problems and think creatively, I am always up-to-date with the latest developments in React and related technologies, and am able to innovate and inspire a positive, collaborative work environment.',
 };
+export const skillsData = {
+  show: true,
+  heading: 'Skills',
+  hardSkills: [
+    { name: 'React', value: 90 },
+    { name: 'JavaScript', value: 90 },
+    { name: 'Data Structures', value: 65 },
+    { name: 'HTML/CSS', value: 55 },
+    { name: 'TypeScript', value: 80 },
+    { name: 'SQL', value: 75 },
+  ],
+  softSkills: [
+    { name: 'Goal-Oriented', value: 80 },
+    { name: 'Collaboration', value: 90 },
+    { name: 'Positivity', value: 75 },
+    { name: 'Adaptability', value: 85 },
+    { name: 'Problem Solving', value: 75 },
+    { name: 'Empathy', value: 90 },
+    { name: 'Organization', value: 70 },
+    { name: 'Creativity', value: 90 },
+  ],
+};
